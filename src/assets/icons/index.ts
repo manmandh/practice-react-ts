e8dcdad1bb749317389f84d7d28df6849f53b339
manmandh <manmantran169@gmail.com>
2025-03-24 23:53:20 +0700

    #1 add: icon and test:counter for storybook #2

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,47 @@
+import ArrowDownCircleIcon from './arrow-down-circle.svg'
+import ArrowLeftIcon from './arrow-left.svg'
+import ArrowRightIcon from './arrow-right.svg'
+import ArrowUpCircleIcon from './arrow-up-circle.svg'
+import CreditCardIcon from './credit-card.svg'
+import EconometricsIcon from './econometrics.svg'
+import EconometricInvestmentIcon from './economic-investment.svg'
+import HomeIcon from './home.svg'
+import BusinessFinanceMoneyIcon from './iconfinder-business-finance-money.svg'
+import PaypalPaymentIcon from './iconfinder-paypal-payment-pay.svg'
+import IconfinderIcon from './iconfinder.svg'
+import LoanIcon from './loan.svg'
+import LogoIcon from './logo.svg'
+import NotificationIcon from './notification.svg'
+import RenewIcon from './renew.svg'
+import SearchIcon from './search.svg'
+import SendIcon from './send.svg'
+import SettingServiceIcon from './settings-service.svg'
+import SettingSolidIcon from './settings-solid.svg'
+import SettingIcon from './settings.svg'
+import TransferIcon from './transfer.svg'
+import UserIcon from './user.svg'
+
+export {
+  ArrowDownCircleIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowUpCircleIcon,
+  CreditCardIcon,
+  EconometricsIcon,
+  EconometricInvestmentIcon,
+  HomeIcon,
+  BusinessFinanceMoneyIcon,
+  PaypalPaymentIcon,
+  IconfinderIcon,
+  LoanIcon,
+  LogoIcon,
+  NotificationIcon,
+  RenewIcon,
+  SearchIcon,
+  SendIcon,
+  SettingServiceIcon,
+  SettingSolidIcon,
+  SettingIcon,
+  TransferIcon,
+  UserIcon
+}
