7e8de3ea9061df41d276dc147e95872bf41f1c70
manmandh <manmantran169@gmail.com>
2025-06-13 00:33:15 +0700

    #9 fix:vercel for product management page #10

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import 'react-toastify/ReactToastify.css'
 import './App.css'
 import HomePage from './pages/home/homePage'
 import { ProductModalProvider } from './context/productModalContext/productModalContext'
-import ProductModal from './Components/common/productForm/productModal'
+import ProductModal from './components/common/productForm/productModal'
+import ProductManagemnentPage from './pages/product-management/productManagementPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <ToastContainer pauseOnFocusLoss={false} hideProgressBar position='top-right' />
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path='/manage/product' element={<ProductManagemnentPage />} />
           <Route
             path='*'
             element={<div className='absolute inset-0 flex items-center justify-center'>Page Not Found</div>}
