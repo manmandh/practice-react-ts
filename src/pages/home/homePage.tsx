5de1a285f8e8b0e8080df99d23d078e6f46ac59a
manmandh <manmantran169@gmail.com>
2025-06-13 00:37:17 +0700

    #9 fix:vercel for product management page #10

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import ProductModal from '../../Components/common/productForm/productModal'
+import ProductModal from '../../components/common/productForm/productModal'
 import ProductModalContext from '../../context/productModalContext/productModalContext'
 
 const HomePage = () => {
